4bc34a0a5c8ec575aa5bd170a49e125b2376e83b
juanpico12 <juanfernandopico12@gmail.com>
2020-03-09 19:23:32 -0300

    Topic 1 Excercise 2

--- 01-JS-DOM-APIs/js.js
+++ 01-JS-DOM-APIs/js.js
@@ -2,3 +2,13 @@
 
 window.onload = () => document.getElementById('hidden').style.opacity = 1;
 
+function clickAlert(){
+    alert("Alerta de Click")
+}
+
+
+let button = document.getElementById('button');
+
+button.onclick = clickAlert;
+
+
