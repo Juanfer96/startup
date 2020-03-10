c4ad1e20557a788652cb0766d094d4c2b3d4d54c
juanpico12 <juanfernandopico12@gmail.com>
2020-03-10 15:05:17 -0300

    Topic 1 Exercise 4

--- 01-JS-DOM-APIs/js.js
+++ 01-JS-DOM-APIs/js.js
@@ -7,6 +7,11 @@ function clickAlert(){
 }
 
 let button = document.getElementById('button');
+let buttonSearch= document.getElementById('button-search');
+
+button.onclick = data;
+buttonSearch.onclick = dataSearch;
+dataSearchJavaScript();
 
 function data(){
     let section= document.getElementById('section');
@@ -23,4 +28,56 @@ function data(){
         }); 
 }
 
-button.onclick = data;
+/* Function to fetch repo with q = JavaScript */
+
+function dataSearchJavaScript(){
+    fetch('https://api.github.com/search/repositories?q=JavaScript')
+        .then(response => {
+            if (!response.ok) throw Error(response.status);
+      
+            return response.json();
+        })
+        .then(data => {
+            let firstLi = document.getElementById("first-li"); 
+            firstLi.innerHTML += 'Lista de Repositorios con "JavaScript"';
+            data.items.forEach(element => {
+                let repo = JSON.stringify(element.html_url);
+                let newLi = document.createElement("li");
+                let newContent = document.createTextNode(repo);
+                newLi.appendChild(newContent);
+                firstLi.insertAdjacentElement('afterend', newLi);
+            })
+        })
+        .catch(error => {
+            console.log(error);
+        }); 
+}
+
+/* Function to fetch repo with with q = input text*/
+
+function dataSearch(){
+    let search = document.getElementById('search').value;
+    search = JSON.stringify(search)
+    fetch('https://api.github.com/search/repositories?q='+search)
+        .then(response => {
+            if (!response.ok) throw Error(response.status);
+      
+            return response.json();
+        })
+        .then(data => {
+            let firstLi = document.getElementById("first-li"); 
+            firstLi.innerHTML ='Lista de Repositorios con '+ search;
+            data.items.forEach(element => {
+                let repo = JSON.stringify(element.html_url);
+                let newLi = document.createElement("li");
+                let newContent = document.createTextNode(repo);
+                newLi.appendChild(newContent);
+                firstLi.insertAdjacentElement('afterend', newLi);
+            })
+        })
+        .catch(error => {
+            console.log(error);
+        }); 
+}
+
+
