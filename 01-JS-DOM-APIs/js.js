621c3761281232298ea9d0cde7db0afd45426231
juanpico12 <juanfernandopico12@gmail.com>
2020-03-10 12:44:49 -0300

    Topic 1 Exercise 3

--- 01-JS-DOM-APIs/js.js
+++ 01-JS-DOM-APIs/js.js
@@ -6,9 +6,21 @@ function clickAlert(){
     alert("Alerta de Click")
 }
 
-
 let button = document.getElementById('button');
 
-button.onclick = clickAlert;
+function data(){
+    let section= document.getElementById('section');
+    fetch('http://api.icndb.com/jokes/random')
+        .then(response => {
+            if (!response.ok) throw Error(response.status);
+      
+            return response.text();
+        })
+         .then(text => section.innerHTML += text) // section.innerHTML = text is OK if I want to change the section content and not append the data response 
+        .catch(error => {
+            console.log(error);
+            section.style.color = '#ff0000';
+        }); 
+}
 
-
+button.onclick = data;
