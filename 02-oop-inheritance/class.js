b550af786b611cc40187c0042d53a122524ba5cd
juanpico12 <juanfernandopico12@gmail.com>
2020-03-13 01:51:47 -0300

    Topic2 Exercise 3

--- 02-oop-inheritance/class.js
+++ 02-oop-inheritance/class.js
@@ -9,7 +9,6 @@ class EventEmitter{
         if( !this.events[eventName] ) {
             this.events[eventName] = [];
          }
-           
          this.events[eventName].push(callback);
     }
 
@@ -19,7 +18,7 @@ class EventEmitter{
             event.forEach(fn => {
                 fn();
             });
-   }
+         }
 
     }
     off(eventName, callback){
@@ -39,11 +38,13 @@ class  Movie extends  EventEmitter{
         this.title= name;
         this.year=year;
         this.duration=duration;
+        this.actors = [];
     }
-  
+    getActors(){
+        return this.actors;
+    }
     play(){
         this.emit('Play');
-    
     }
 
     pause(){
@@ -54,6 +55,13 @@ class  Movie extends  EventEmitter{
     resume(){
         this.emit('Resume');
     }
+    addCast(cast){
+        if(cast.length !== undefined){
+            cast.forEach(element  => this.actors.push(element));//Many Actors
+        } 
+        else    
+            this.actors.push(cast); // One Actor
+    }
     
 }
 class Actor{
@@ -62,18 +70,37 @@ class Actor{
         this.age=age;
     }
 }
+class Logger{
+    constructor(){}
+
+    log(info){
+        console.log(info);
+    }
+
+}
 
 
 /* SOME  RANDOMS TEST  */
 
 let  ironman  =  new  Movie('Ironman','2015','140');
-ironman.on('Play',fn =>  console.log('Playing '+ ironman.title ));
-ironman.on('Pause',fn =>  console.log(ironman.title + ' is paused'));
-ironman.on('Resume',fn =>  console.log('Resume '+ ironman.title ));
 
-ironman.play();
-ironman.pause();
-ironman.resume();
+const arnold = new Actor('Arnold Schwarzenegger', 50);
+const actors = [
+    new Actor('Paul Winfield', 50),
+    new Actor('Michael Biehn', 50),
+    new Actor('Linda Hamilton', 50)
+];
+
+ironman.addCast(arnold);
+console.log(ironman.getActors());
+ironman.addCast(actors);
+console.log(ironman.getActors());
+
+let logg = new Logger();
+
+ironman.on('Play', () => logg.log('The "play" event has been emitted'))
+ironman.play() // output  'The "play" event has been emitted'
+
 
 
 
